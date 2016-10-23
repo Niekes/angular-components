ac1e36ed982e55ef0a7887703ad614e950c5188b
Stefan Nieke <info@niekes.com>
2016-10-24 01:00:04 +0200

    setup

--- gulpfile.js
+++ gulpfile.js
@@ -13,14 +13,14 @@ gulp.task('sass', function(){
     return gulp.src('scss/*.scss')
     .pipe(sass().on('error', sass.logError))
 	.pipe(autoprefixer())
-    .pipe(gulp.dest('angular-components/css/'))
+    .pipe(gulp.dest('css/'))
     .pipe(livereload());
 });
 
 //-------------------------------------------------
 // JAVASCRIPT
 //-------------------------------------------------
-var jsSrc = ['angular-components/js/**/*.js'];
+var jsSrc = ['js/**/*.js'];
 gulp.task('scripts', function(){
     gulp.src(jsSrc)
     .pipe(livereload());
@@ -29,7 +29,7 @@ gulp.task('scripts', function(){
 //-------------------------------------------------
 // HTML
 //-------------------------------------------------
-var htmlSrc = ['angular-components/index.html', 'angular-components/partials/**/*.html'];
+var htmlSrc = ['index.html', 'partials/**/*.html'];
 gulp.task('html', function(){
     gulp.src(htmlSrc)
     .pipe(livereload());
