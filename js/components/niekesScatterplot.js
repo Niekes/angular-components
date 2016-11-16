7e430fc3c72e3d9b71a84698125a75da30b69fa6
Stefan Nieke <info@niekes.com>
2016-11-16 22:24:07 +0100

    cleanup

--- js/components/niekesScatterplot.js
+++ js/components/niekesScatterplot.js
@@ -6,6 +6,7 @@ app.component('niekesScatterPlot', {
   	},
   	controllerAs: 'niekesScatterplotCtrl',
 	controller: function($element){
+
 		var niekesScatterplotCtrl = this;
 		var el = $element[0];
 		var svg;
