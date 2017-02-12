c554c0941e5bf2ff18af7f62eae47c57fc4d881a
Stefan Nieke <info@niekes.com>
2017-02-12 21:58:22 +0100

    fix path issue with images

--- js/controllers/histogramCtrl.js
+++ js/controllers/histogramCtrl.js
@@ -5,12 +5,12 @@ app.controller('histogramCtrl', function(){
 	var $histogramCtrl = this;
 
     $histogramCtrl.images = [
-      	{name:'Cameraman', src: '../../img/cameraman.jpg'},
-      	{name:'Lena', src: '../../img/lena.jpg'},
-      	{name:'Airplane', src: '../../img/airplane.jpg'},
-      	{name:'Peppers', src: '../../img/peppers.jpg'},
-      	{name:'Lake', src: '../../img/lake.jpg'},
-      	{name:'Baboon', src: '../../img/baboon.jpg'},
+      	{name:'Cameraman', src: './../../img/cameraman.jpg'},
+      	{name:'Lena', src: './../../img/lena.jpg'},
+      	{name:'Airplane', src: './../../img/airplane.jpg'},
+      	{name:'Peppers', src: './../../img/peppers.jpg'},
+      	{name:'Lake', src: './../../img/lake.jpg'},
+      	{name:'Baboon', src: './../../img/baboon.jpg'},
     ];
 
     $histogramCtrl.defaultImage = $histogramCtrl.images[0];
