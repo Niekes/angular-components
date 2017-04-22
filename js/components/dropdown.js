7a4124b51fb892150bca986b401b4b7685c0f23c
Stefan Nieke <info@niekes.com>
2017-04-23 00:04:22 +0200

    add de/selectAll

--- js/components/dropdown.js
+++ js/components/dropdown.js
@@ -12,7 +12,14 @@ app.component('dropdown', {
 		var $dropdownCtrl = this;
 		var el = $element[0];
 		var tt = DEFAULTS.TRANSITION.TIME;
+		var sym = {
+			plus: '&#8853',
+			minus: '&#8854',
+			selectAll: '&#9744;',
+			deselectAll: '&#9745;'
+		};
 		var key = function(d){ return d.name; };
+		var hidden = false;
 
 		$dropdownCtrl.init = function(){
 
@@ -38,6 +45,10 @@ app.component('dropdown', {
 				select(this);
 			});
 
+			d3.selectAll('span.selectAll').on('click', function(){
+				selectAll(this);
+			});
+
 			d3.selectAll('span.expand').on('click', function(){
 				expandChilds(this);
 			});
@@ -55,13 +66,14 @@ app.component('dropdown', {
 				.each(function(d){ d.depth = depth; })
 				.html(function(d){
 					var cssC = d.children !== undefined ? 'it' : 'it sin';
-					var html = '<span class="'+cssC+'">' + d.name + '</span>';
+					var html = '<span data-depth="'+depth+'" class="'+cssC+'">' + d.name + '</span>';
+					var expandSym = hidden ? sym.plus : sym.minus;
 					if(d.children !== undefined){
-						html += '<span class="selectAll">&#9744;</span><span class="expand">&#8853;</span>';
+						html += '<span class="selectAll">&#9744;</span><span class="expand">'+ expandSym +'</span>';
 					}
 					return html;
 				})
-				.append('ul').attr('class', 'childList').classed('hidden', true);
+				.append('ul').attr('class', 'childList').classed('hidden', hidden);
 
 			parent
 				.exit()
@@ -79,8 +91,28 @@ app.component('dropdown', {
 			}
 		}
 
-		function select(){
+		function select(el){
+			var _el = d3.select(el);
+			var _hh = _el.classed('selected');
+			_el.classed('selected', !_hh);
+		}
 
+		function selectAll(el){
+			var _el = d3.select(el);
+			var _ch = d3.select(_el.node().nextSibling);
+			var _dh = d3.select(_ch.node().nextSibling);
+			var _sp = _dh.node().getElementsByClassName('it');
+			var _de = _dh.node().__data__.depth + 1;
+			var _se = _el.html().charCodeAt(0) === 9744 ? true : false;
+
+			_el.html(_se ? sym.deselectAll : sym.selectAll);
+
+			for (var i = 0; i < _sp.length; ++i) {
+				var _d = +d3.select(_sp[i]).attr('data-depth');
+				if(_d === _de){
+					d3.select(_sp[i]).classed('selected', _se);
+				}
+			}
 		}
 
 		function expandChilds(el){
